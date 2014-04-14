64efcca5d2c950148b353a94af9266c122265e8f
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-04-14 12:19:54 +0200

    stash-like wip

--- plugins/sigma.layout.workerForce/force.js
+++ plugins/sigma.layout.workerForce/force.js
@@ -1,30 +1,16 @@
 ;(function(global) {
+
+
   'use strict';
 
-  var MovingPoint = (function() {
-    function MovingPoint(id, x, y) {
-      this.id = id;
-      this.x = x;
-      this.y = y;
-      if (!Number.isFinite(x))
-        this.x = Math.floor(1000 * Math.random());
-      if (!Number.isFinite(y))
-        this.y = Math.floor(1000 * Math.random());
-
-      this.speedX = this.speedY = 0;
-    }
-    return MovingPoint;
-  })();
-
-  if (typeof this.performance !== 'object') {
+  if (typeof this.performance !== 'object')
     this.performance = {
       now: Date.now
     };
-  }
 
   var sendNewCoords;
 
-  if (Object(this.document) === this.document) {
+  if (Object(this.document) === this.document)
     sendNewCoords = function sendNewCoords(coords) {
       var e = new Event('newCoords');
       e.coords = coords;
@@ -32,14 +18,13 @@
         return document.dispatchEvent(e);
       });
     };
-  } else {
-    if (typeof postMessage === 'function') {
+  else {
+    if (typeof postMessage === 'function')
       sendNewCoords = function sendNewCoords(coords) {
         postMessage(coords);
       };
-    } else {
+    else
       throw new Error('No idea of how to reach back the context where the graph is drawn');
-    }
   }
 
   var REPULSION = 700;
@@ -61,16 +46,11 @@
   });
 
   function applyGraphChanges(changes) {
-    if (changes.addNodes) {
-      changes.addNodes.forEach(function(p) {
-        points.set(p.id, new MovingPoint(p.id, p.x, p.y));
-      });
-    }
-    if (changes.addEdges) {
-      changes.addEdges.forEach(function(e) {
-        edges.add({ from: points.get(e.from), to: points.get(e.to) });
-      });
-    }
+    if (changes.addNodes)
+      changes.addNodes.forEach(graph.addNode, graph);
+
+    if (changes.addEdges)
+      changes.addEdges.forEach(graph.addEdge, graph);
 
     (function loop() {
       var newCoords = step();
@@ -82,24 +62,22 @@
   var step = function step() {
     var times = [performance.now()];
 
-    points.forEach(function(p) {
-      var deltaX = CENTER_X - p.x;
-      var deltaY = CENTER_Y - p.y;
-
-      var distanceToCenter = Math.hypot(deltaX, deltaY);
+    graph.nodes().forEach(function(p) {
+      var deltaX = CENTER_X - p.x,
+          deltaY = CENTER_Y - p.y,
+          distanceToCenter = Math.hypot(deltaX, deltaY);
 
       p.accX = CENTER_FORCE * deltaX;
       p.accY = CENTER_FORCE * deltaY;
     });
 
-    times.push(performance.now());
     var pointsArray = [],
         top = +Infinity,
         right = -Infinity,
         bottom = -Infinity,
         left = +Infinity;
 
-    points.forEach(function(p) {
+    graph.nodes().forEach(function(p) {
       pointsArray.push(p);
       if (p.y < top)
         top = p.y - 0.1;
@@ -111,13 +89,9 @@
         right = p.x + 0.1;
     });
 
-    times.push(performance.now());
-
     bspTree = new BSPTree(top, right, bottom, left, pointsArray);
 
-    times.push(performance.now());
-
-    points.forEach(function(p) {
+    graph.nodes().forEach(function(p) {
       var repulsionX = 0,
           repulsionY = 0,
           forceRelevantMassedPoints = bspTree.getForceRelevantMassedPoints(p.x, p.y);
@@ -138,41 +112,35 @@
       p.accY -= REPULSION * repulsionY;
     });
 
-    times.push(performance.now());
-
-    edges.forEach(function(e) {
-      var from = e.from,
-          to = e.to,
-          deltaX = to.x - from.x,
-          deltaY = to.y - from.y,
+    graph.edges().forEach(function(e) {
+      var source = graph.nodes(e.source),
+          target = graph.nodes(e.target),
+          deltaX = target.x - source.x,
+          deltaY = target.y - source.y,
           distance = Math.hypot(deltaX, deltaY),
           attractX = ATTRACTION * (distance - IDEAL_EDGE_DISTANCE) * deltaX;
           attractY = ATTRACTION * (distance - IDEAL_EDGE_DISTANCE) * deltaY;
 
-      from.accX += attractX;
-      from.accY += attractY;
-      to.accX -= attractX;
-      to.accY -= attractY;
+      source.accX += attractX;
+      source.accY += attractY;
+      target.accX -= attractX;
+      target.accY -= attractY;
     });
-
-    times.push(performance.now());
 
     var newCoords = [];
 
-    points.forEach(function(p) {
-      p.speedX = p.speedX * FRICTION + G * p.accX;
-      p.speedY = p.speedY * FRICTION + G * p.accY;
+    graph.nodes().forEach(function(p) {
+      n.speedX = n.speedX * FRICTION + G * n.accX;
+      n.speedY = n.speedY * FRICTION + G * n.accY;
 
-      p.x += p.speedX;
-      p.y += p.speedY;
+      n.x += n.speedX;
+      n.y += n.speedY;
 
-      newCoords.push({ id: p.id, x: p.x, y: p.y });
-    });
-
-    times.push(performance.now());
-
-    times = times.map(function(t, i) {
-      return i === times.length - 1 ? undefined : times[i + 1] - t;
+      newCoords.push({
+        id: n.id,
+        x: n.x,
+        y: n.y
+      });
     });
 
     return newCoords;
