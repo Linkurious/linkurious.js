7cc90e27542f72ed2c4c93925b1cc6bb930912c2
Sébastien Heymann <seb@linkurio.us>
2015-07-06 16:48:17 +0200

    Add sigma.layout.dagre

---
+++ plugins/sigma.layout.dagre/README.md
@@ -0,0 +1,92 @@
+sigma.layout.dagre
+========================
+
+Plugin developed by [Sébastien Heymann](https://github.com/sheymann) for [Linkurious](https://github.com/Linkurious) and published under the [MIT](LICENSE) license.
+
+Contact: seb@linkurio.us
+
+---
+
+This plugin implements a binding to the [dagre layout](), which organises the graph using a DAG (directed acyclic graph) system, written by [Chris Pettitt](https://www.linkedin.com/in/chrismpettitt). It is especially suitable for DAGs and trees. For more information, please refer to its [Dagre's documentation](https://github.com/cpettitt/dagre).
+
+Because Dagre is a separate library, the developer must manually include `dagre.js` in their app in order to use the layout.
+
+## Recommended reading
+
+source: https://github.com/cpettitt/dagre/wiki#recommended-reading
+
+This work was produced by taking advantage of many papers and books. If you're interested in how dagre works internally here are some of the most important papers to read.
+
+The general skeleton for Dagre comes from [*Gansner, et al., "A Technique for Drawing Directed Graphs"*](http://www.graphviz.org/Documentation/TSE93.pdf), which gives both an excellent high level overview of the phases involved in layered drawing as well as diving into the details and problems of each of the phases. Besides the basic skeleton, the author specifically used the technique described in the paper to produce an acyclic graph, and the author use the network simplex algorithm for ranking. If there is one paper to start with when learning about layered graph drawing, this is it!
+
+For crossing minimization we used [*Jünger and Mutzel, "2-Layer Straightline Crossing Minimization"*](http://www.researchgate.net/profile/Petra_Mutzel/publication/30508315_2-Layer_Straightline_Crossing_Minimization_Performance_of_Exact_and_Heuristic_Algorithms/links/09e4150eabaf4cc7bd000000.pdf), which provides a comparison of the performance of various heuristics and exact algorithms for crossing minimization.
+
+For counting the number of edge crossings between two layers we use the `O(|E| log |V_small|)` algorithm described in [*Barth, et al., "Simple and Efficient Bilayer Cross Counting"*](http://jgaa.info/accepted/2004/BarthMutzelJuenger2004.8.2.pdf).
+
+For positioning (or coordinate assignment), we derived our algorithm from [*Brandes and Köpf, "Fast and Simple Horizontal Coordinate Assignment"*](http://www.inf.uni-konstanz.de/algo/publications/bk-fshca-01.pdf). The author made some adjustments to get tighter graphs when node and edges sizes vary greatly.
+
+The implementation for clustering derives extensively from Sander, [*"Layout of Compound Directed Graphs."*](http://scidok.sulb.uni-saarland.de/volltexte/2005/359/pdf/tr-A03-96.pdf) It is an excellent paper that details the impact of clustering on all phases of layout and also covers many of the associated problems. Crossing reduction with clustered graphs derives from two papers by Michael Forster, *"Applying Crossing Reduction Strategies to Layered Compound Graphs"* and *"A Fast and Simple Heuristic for Constrained Two-Level Crossing Reduction."*
+
+## Methods
+
+**configure**
+
+Changes the layout's configuration.
+
+```js
+var listener = sigma.layouts.dagre.configure(sigInst, config);
+```
+
+**start**
+
+Starts the layout. It is possible to pass a configuration if this is the first time you start the layout.
+
+```js
+var listener = sigma.layouts.dagre.start(sigInst, config);
+```
+
+**isRunning**
+
+Returns whether the layout is running.
+
+```js
+sigma.layouts.dagre.isRunning(sigInst);
+```
+
+## Configuration
+
+* **nodes**: *array*: the subset of nodes to apply the layout.
+* **boundingBox** *object|boolean*: constrain layout bounds. Value: {minX, maxX, minY, maxY} or true (all current positions of the given nodes).
+
+*Algorithm configuration*
+
+* **directed**: *boolean* `true`: if `true`, take edge direction into account.
+* **multigraph**: *boolean* `true`: if `true`, allows multiple edges between the same pair of nodes.
+* **compound**: *boolean* `true`: if `true`, allows ompound nodes, i.e. nodes which can be the parent of other nodes.
+* **rankDir** *string* `TB`: direction for rank nodes. Can be `TB`, `BT`, `LR`, or `RL`, where T = top, B = bottom, L = left, and R = right.
+
+*Easing configuration*
+
+* **easing** *string*: if specified, ease the transition between nodes positions if background is `true`. The duration is specified by the Sigma settings `animationsTime`. See [sigma.utils.easing](../../src/utils/sigma.utils.js#L723) for available values.
+* **duration** *number*: duration of the transition for the easing method. Default value is Sigma setting `animationsTime`.
+
+## Events
+
+The plugin dispatches the following events:
+
+- `start`: on layout start.
+- `interpolate`: at the beginning of the layout animation if an *easing* function is specified and the layout is ran on background.
+- `stop`: on layout stop, will be dispatched after `interpolate`.
+
+Example:
+
+```js
+// Start the algorithm:
+var listener = sigma.layouts.dagre.configure(sigInst, config);
+// Bind all events:
+listener.bind('start stop interpolate', function(event) {
+  console.log(event.type);
+});
+
+sigma.layouts.dagre.start(sigInst);
+```
---
+++ plugins/sigma.layout.dagre/sigma.layout.dagre.js
@@ -0,0 +1,379 @@
+;(function(undefined) {
+  'use strict';
+
+  if (typeof sigma === 'undefined')
+    throw new Error('sigma is not declared');
+
+  if (typeof dagre === 'undefined')
+    throw new Error('dagre is not declared');
+
+  if (typeof dagre.graphlib === 'undefined')
+    throw new Error('dagre.graphlib is not declared');
+
+  // Initialize package:
+  sigma.utils.pkg('sigma.layouts.dagre');
+
+  /**
+   * Sigma Dagre layout
+   * ===============================
+   *
+   * Require https://github.com/cpettitt/dagre
+   * Author: Sébastien Heymann @ Linkurious
+   * Version: 0.1
+   */
+
+
+  // see https://github.com/cpettitt/dagre/wiki#configuring-the-layout
+  var settings = {
+    directed: true, // take edge direction into account
+    multigraph: true, // allows multiple edges between the same pair of nodes
+    compound: false, //
+
+    // dagre algo options
+    rankDir: 'TB', // Direction for rank nodes. Can be TB, BT, LR, or RL,
+                   // where T = top, B = bottom, L = left, and R = right.
+  };
+
+  var _instance = {};
+
+
+  /**
+   * Event emitter Object
+   * ------------------
+   */
+  var _eventEmitter = {};
+
+
+  function getBoundaries(nodes, prefix) {
+    var i,
+        l,
+        prefix = prefix || '',
+        sizeMax = -Infinity,
+        minX = Infinity,
+        minY = Infinity,
+        maxX = -Infinity,
+        maxY = -Infinity;
+
+    for (i = 0, l = nodes.length; i < l; i++) {
+      sizeMax = Math.max(nodes[i][prefix + 'size'], sizeMax);
+      maxX = Math.max(nodes[i][prefix + 'x'], maxX);
+      minX = Math.min(nodes[i][prefix + 'x'], minX);
+      maxY = Math.max(nodes[i][prefix + 'y'], maxY);
+      minY = Math.min(nodes[i][prefix + 'y'], minY);
+    }
+
+    sizeMax = sizeMax || 1;
+
+    return {
+      sizeMax: sizeMax,
+      minX: minX,
+      minY: minY,
+      maxX: maxX,
+      maxY: maxY
+    };
+  };
+
+  function scaleRange (value, baseMin, baseMax, limitMin, limitMax) {
+    return ((limitMax - limitMin) * (value - baseMin) / (baseMax - baseMin)) + limitMin;
+  };
+
+  function rescalePosition(point, baseBox, limitBox) {
+    return {
+      x: scaleRange(point.x, baseBox.minX, baseBox.maxX, limitBox.minX, limitBox.maxX),
+      y: scaleRange(point.y, baseBox.minY, baseBox.maxY, limitBox.minY, limitBox.maxY)
+    }
+  };
+
+  /**
+   * Dagre Object
+   * ------------------
+   */
+  function dagreLayout() {
+    var
+      self = this,
+      dg;
+
+    this.init = function (sigInst, options) {
+      options = options || {};
+
+      if (options.nodes) {
+        this.nodes = options.nodes;
+        delete options.nodes;
+      }
+
+      if (options.boundingBox) {
+        this.boundingBox = options.boundingBox;
+        delete options.boundingBox;
+      }
+
+      // Properties
+      this.sigInst = sigInst;
+      this.config = sigma.utils.extend(options, settings);
+      this.easing = options.easing;
+      this.duration = options.duration;
+
+      if (this.easing && (!sigma.plugins || typeof sigma.plugins.animate === 'undefined')) {
+        throw new Error('sigma.plugins.animate is not declared');
+      }
+
+      // State
+      this.started = false;
+      this.running = false;
+    };
+
+    this.start = function() {
+      if (this.running) return;
+
+      this.running = true;
+
+      if (!this.started) {
+        // Create a new directed graph
+        dg = new dagre.graphlib.Graph({
+          directed: this.config.directed,
+          multigraph: this.config.multigraph,
+          compound: this.config.compound
+        });
+
+        // Set an object for the graph label
+        dg.setGraph(this.config);
+
+        var nodes = this.nodes || this.sigInst.graph.nodes();
+        for (var i = 0; i < nodes.length; i++) {
+          if (!nodes[i].fixed) {
+            dg.setNode(nodes[i].id, {});
+          }
+        }
+
+        if (this.boundingBox === true) {
+          this.boundingBox = getBoundaries(nodes);
+        }
+
+        var edges = this.sigInst.graph.edges();
+        for (var i = 0; i < edges.length; i++) {
+          if (dg.node(edges[i].source) != null && dg.node(edges[i].target) != null) {
+            dg.setEdge(edges[i].source, edges[i].target, { id: edges[i].id });
+          }
+        };
+
+        this.started = true;
+        _eventEmitter[self.sigInst.id].dispatchEvent('start');
+
+        // console.time('sigma.layout.dagre');
+        dagre.layout(dg);
+        // console.timeEnd('sigma.layout.dagre');
+
+        var edge;
+        dg.edges().map(function(e) {
+          edge = self.sigInst.graph.edges(dg.edge(e).id);
+          edge.points = dg.edge(e).points;
+        });
+
+        this.stop();
+      }
+    };
+
+    this.stop = function() {
+      if (!dg) return;
+
+      var nodes = dg.nodes().map(function(nid) {
+        return self.sigInst.graph.nodes(nid);
+      });
+
+      var coord;
+
+      if (this.boundingBox) {
+        var baseBoundingBox = getBoundaries(dg.nodes().map(function(nid) {
+          return dg.node(nid);
+        }));
+      }
+
+      this.running = false;
+
+      if (this.easing) {
+        // Set new node coordinates
+        for (var i = 0; i < nodes.length; i++) {
+          if (this.boundingBox) {
+            coord = rescalePosition(dg.node(nodes[i].id), baseBoundingBox, self.boundingBox);
+            nodes[i].dagre_x = coord.x;
+            nodes[i].dagre_y = coord.y;
+          }
+          else {
+            nodes[i].dagre_x = dg.node(nodes[i].id).x;
+            nodes[i].dagre_y = dg.node(nodes[i].id).y;
+          }
+        }
+
+        _eventEmitter[self.sigInst.id].dispatchEvent('interpolate');
+        sigma.plugins.animate(
+          self.sigInst,
+          {
+            x: 'dagre_x',
+            y: 'dagre_y'
+          },
+          {
+            nodes: nodes,
+            easing: self.easing,
+            onComplete: function() {
+              for (var i = 0; i < nodes.length; i++) {
+                nodes[i].dagre_x = null;
+                nodes[i].dagre_y = null;
+              }
+              _eventEmitter[self.sigInst.id].dispatchEvent('stop');
+              self.sigInst.refresh();
+            },
+            duration: self.duration
+          }
+        );
+      }
+      else {
+        // Apply changes
+        var node;
+        dg.nodes().forEach(function(nid) {
+          node = self.sigInst.graph.nodes(nid);
+          node.x = dg.node(nid).x;
+          node.y = dg.node(nid).y;
+        });
+
+        _eventEmitter[self.sigInst.id].dispatchEvent('stop');
+        this.sigInst.refresh();
+      }
+    };
+
+    this.kill = function() {
+      this.sigInst = null;
+      this.config = null;
+      this.easing = null;
+    };
+  };
+
+
+
+  /**
+   * Interface
+   * ----------
+   */
+
+  /**
+   * Configure the layout algorithm.
+
+   * Recognized options:
+   * **********************
+   * Here is the exhaustive list of every accepted parameters in the settings
+   * object:
+   *
+   *   {?array}             nodes      The subset of nodes to apply the layout.
+   *   {?object}           boundingBox Constrain layout bounds. Value: {minX, maxX, minY, maxY}
+   *                                   or true (all current positions of the given nodes)
+   *   {?boolean}           directed   If `true`, take edge direction into
+   *                                   account. Default: `true`.
+   *   {?boolean}           multigraph If `true`, allows multiple edges between
+   *                                   the same pair of nodes. Default: `true`.
+   *   {?boolean}           compound   If `true`, allows ompound nodes, i.e.
+   *                                   nodes which can be the parent of other
+   *                                   nodes. Default: `false`.
+   *   {?string}            rankDir    Direction for rank nodes. Can be TB, BT,
+   *                                   LR, or RL, where T = top, B = bottom,
+   *                                   L = left, and R = right.
+   *   {?(function|string)} easing     Either the name of an easing in the
+   *                                   sigma.utils.easings package or a
+   *                                   function. If not specified, the
+   *                                   quadraticInOut easing from this package
+   *                                   will be used instead.
+   *   {?number}            duration   The duration of the animation. If not
+   *                                   specified, the "animationsTime" setting
+   *                                   value of the sigma instance will be used
+   *                                   instead.
+   *
+   *
+   * @param  {sigma}   sigInst The related sigma instance.
+   * @param  {object} config  The optional configuration object.
+   *
+   * @return {sigma.classes.dispatcher} Returns an event emitter.
+   */
+  sigma.layouts.dagre.configure = function(sigInst, config) {
+    if (!sigInst) throw new Error('Missing argument: "sigInst"');
+    if (!config) throw new Error('Missing argument: "config"');
+
+    // Create instance if undefined
+    if (!_instance[sigInst.id]) {
+      _instance[sigInst.id] = new dagreLayout();
+
+      _eventEmitter[sigInst.id] = {};
+      sigma.classes.dispatcher.extend(_eventEmitter[sigInst.id]);
+
+      // Binding on kill to clear the references
+      sigInst.bind('kill', function() {
+        _instance[sigInst.id].kill();
+        _eventEmitter[sigInst.id] = null;
+      });
+    }
+
+    _instance[sigInst.id].init(sigInst, config);
+
+    return _eventEmitter[sigInst.id];
+  };
+
+  /**
+   * Start the layout algorithm. It will use the existing configuration if no
+   * new configuration is passed.
+
+   * Recognized options:
+   * **********************
+   * Here is the exhaustive list of every accepted parameters in the settings
+   * object:
+   *
+   *   {?array}             nodes      The subset of nodes to apply the layout.
+   *   {?object}           boundingBox Constrain layout bounds. Value: {minX, maxX, minY, maxY}
+   *                                   or true (all current positions of the given nodes).
+   *   {?boolean}           directed   If `true`, take edge direction into
+   *                                   account. Default: `true`.
+   *   {?boolean}           multigraph If `true`, allows multiple edges between
+   *                                   the same pair of nodes. Default: `true`.
+   *   {?boolean}           compound   If `true`, allows ompound nodes, i.e.
+   *                                   nodes which can be the parent of other
+   *                                   nodes. Default: `false`.
+   *   {?string}            rankDir    Direction for rank nodes. Can be TB, BT,
+   *                                   LR, or RL, where T = top, B = bottom,
+   *                                   L = left, and R = right.
+   *   {?(function|string)} easing     Either the name of an easing in the
+   *                                   sigma.utils.easings package or a
+   *                                   function. If not specified, the
+   *                                   quadraticInOut easing from this package
+   *                                   will be used instead.
+   *   {?number}            duration   The duration of the animation. If not
+   *                                   specified, the "animationsTime" setting
+   *                                   value of the sigma instance will be used
+   *                                   instead.
+   *
+   *
+   * @param  {sigma}   sigInst The related sigma instance.
+   * @param  {?object} config  The optional configuration object.
+   *
+   * @return {sigma.classes.dispatcher} Returns an event emitter.
+   */
+  sigma.layouts.dagre.start = function(sigInst, config) {
+    if (!sigInst) throw new Error('Missing argument: "sigInst"');
+
+    if (config) {
+      this.configure(sigInst, config);
+    }
+
+    _instance[sigInst.id].start();
+
+    return _eventEmitter[sigInst.id];
+  };
+
+  /**
+   * Returns true if the layout has started and is not completed.
+   *
+   * @param  {sigma}   sigInst The related sigma instance.
+   *
+   * @return {boolean}
+   */
+  sigma.layouts.dagre.isRunning = function(sigInst) {
+    if (!sigInst) throw new Error('Missing argument: "sigInst"');
+
+    return !!_instance[sigInst.id] && _instance[sigInst.id].running;
+  };
+
+}).call(this);
