e47065730f0f28d61e1e99c61d754a2376ddcd8e
Damien <damien@dam.io>
2015-07-28 13:47:41 +0200

    a few more fixes in the plugins

--- plugins/sigma.renderers.linkurious/canvas/sigma.canvas.edgehovers.def.js
+++ plugins/sigma.renderers.linkurious/canvas/sigma.canvas.edgehovers.def.js
@@ -101,7 +101,8 @@
     // draw label with a background
     if (sigma.canvas.edges.labels.def) {
       edge.hover = true;
-      sigma.canvas.edges.labels.def(edge, source, target, context, settings);
+      var def = sigma.canvas.edges.labels.def;
+      (def.render || def)(edge, source, target, context, settings);
       edge.hover = false;
     }
   };
