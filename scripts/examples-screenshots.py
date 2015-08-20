b4b261c4e5de9e71688f87e1b715d71aa8be2cee
Damien <damien@dam.io>
2015-08-20 11:31:28 +0200

    move examples gallery back to examples/index.html because link is already in the wild

--- scripts/examples-screenshots.py
+++ scripts/examples-screenshots.py
@@ -1,8 +1,10 @@
-#! /usr/bin/env python3
+#! /usr/bin/python3
 """
 Take screenshots of all the examples
 
 Use `python3 -m http.server` to serve your files
+
+You need to remove examples/index.html for files to be listed
 
 Dependencies:
   - python 3
@@ -10,11 +12,11 @@ Dependencies:
   - firefox
 """
 
-INDEX_URL = "http://127.0.0.1:8000/examples"
-DESTINATION = '../examples/screens/'
-
 from selenium import webdriver
 import time, re, os.path
+
+INDEX_URL = "http://127.0.0.1:8000/examples"
+DESTINATION = '../examples/screens/' if sys.
 
 driver = webdriver.Firefox()
 driver.get(INDEX_URL)
@@ -22,7 +24,7 @@ driver.get(INDEX_URL)
 #find the links to the examples
 l = driver.find_elements_by_tag_name('a')
 l = [x.get_attribute('href') for x in l]
-l = [x for x in l if x.endswith('.html') and '/list.html' not in x]
+l = [x for x in l if x.endswith('.html') and '/index.html' not in x]
 
 print(len(l),'links found')
 
