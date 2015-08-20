4ff3d3d6862e22820fffec3c3254899c1fdf8375
Damien <damien@dam.io>
2015-08-20 18:40:30 +0200

    #219: Add error checking script for all the examples

--- scripts/check-errors.py
+++ scripts/check-errors.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 """
-Take screenshots of all the examples
+Check for errors on all the pages
 
 Use `python3 -m http.server` to serve your files
 
@@ -11,23 +11,29 @@ Dependencies:
 """
 from selenium import webdriver
 import time, os
+from os.path import isfile, join
 
 SERVER_URL = "http://127.0.0.1:8000/examples/"
 EXAMPLES_DIR = '../examples'
-DESTINATION = EXAMPLES_DIR+'/screens'
+#DESTINATION = 'screens/'
 
 driver = webdriver.Firefox()
 
+#empty the useless messages
+for entry in driver.get_log('browser'):
+	pass
+
 #find the links to the examples
-from os import listdir
-from os.path import isfile, join
-l = [ f for f in listdir(EXAMPLES_DIR) if isfile(join(EXAMPLES_DIR,f)) \
+l = [ f for f in os.listdir(EXAMPLES_DIR) if isfile(join(EXAMPLES_DIR,f)) \
 	and '.html' in f and 'index.html' not in f]
 
 for a in l:
     name = a.replace('.html','')
     print(name)
     driver.get(SERVER_URL+a) #go to page
-    time.sleep(4) #wait for things to setup
-    driver.save_screenshot(DESTINATION+name+'.png')
+    time.sleep(0.2) #wait for things to setup
+    for entry in driver.get_log('browser'):
+        if entry['level'] not in ('INFO','WARNING'):
+            print(entry['level'],entry['message'])
+    #driver.save_screenshot(DESTINATION+name+'.png')
 driver.quit()
