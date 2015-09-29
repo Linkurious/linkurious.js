312efb445721282b275256afa84ea66f9a74d17a
Ytawo <milan.sylvain@gmail.com>
2015-09-29 14:08:52 +0200

    #662 README.

--- plugins/sigma.exporters.graphml/sigma.exporters.graphml.js
+++ plugins/sigma.exporters.graphml/sigma.exporters.graphml.js
@@ -79,6 +79,21 @@
     return str.split('.').reduce(function(obj, i) { return obj[i] }, obj);
   }
 
+  function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+      parseInt(result[1], 16),
+      parseInt(result[2], 16),
+      parseInt(result[3], 16)
+    ] : null;
+  }
+
   sigma.prototype.toGraphML = function (params) {
     params = params || {};
 
@@ -97,21 +112,6 @@
         params.renderer.camera.readPrefix;
     } else {
       prefix = '';
-    }
-
-    function hexToRgb(hex) {
-      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-      hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-        return r + r + g + g + b + b;
-      });
-
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? [
-        parseInt(result[1], 16),
-        parseInt(result[2], 16),
-        parseInt(result[3], 16)
-      ] : null;
     }
 
     function setRGB(obj, color) {
