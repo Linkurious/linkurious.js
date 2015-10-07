3038bed7eebb21a6b53af16369406ac3770c0d86
Ytawo <milan.sylvain@gmail.com>
2015-10-07 11:15:11 +0200

    Started legend plugin.

---
+++ plugins/sigma.plugins.legend/sigma.plugins.legend.js
@@ -0,0 +1,9 @@
+;(function(undefined) {
+  'use strict';
+
+  if (typeof sigma === 'undefined')
+    throw 'sigma is not declared';
+
+  // Initialize package:
+  sigma.utils.pkg('sigma.plugins');
+}).call(this);
