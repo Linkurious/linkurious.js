94a11fa31f0f4af337cd04ab152ac05e3ca0560c
Sébastien Heymann <seb@linkurio.us>
2015-10-15 18:13:40 +0200

    #272 Refactor autoCurve

--- plugins/sigma.renderers.linkurious/canvas/sigma.canvas.edges.autoCurve.js
+++ plugins/sigma.renderers.linkurious/canvas/sigma.canvas.edges.autoCurve.js
@@ -3,25 +3,48 @@
 
   sigma.utils.pkg('sigma.canvas.edges');
 
+
+  var calc = function(ratio, n, i, sortByDirection) {
+    if (sortByDirection) {
+      // sort edges by direction:
+      var d = (ratio * n) / i;
+      return { y: d ? d : Number.POSITIVE_INFINITY };
+    }
+
+    var step = ratio / (n / 2);
+    var d = ratio - step * i;
+    return { y: d ? 1 / d : n };
+  };
+
   /**
    * Curves multiple edges between two nodes (i.e. "parallel edges").
    * This method is not a renderer. It should be called after modification
    * of the graph structure.
    * Time complexity: 2 * O(|E|)
    *
-   * @param  {object}   s      The sigma instance
-   * @param  {?number}  ratio  The curvature ratio (default: 0.125)
+   * Settings: autoCurveRatio, autoCurveSortByDirection
+   *
+   * @param {object} s The sigma instance
    */
-  sigma.canvas.edges.autoCurve = function(s, ratio) {
+  sigma.canvas.edges.autoCurve = function(s) {
+    var
+      key,
+      ratio = s.settings('autoCurveRatio'),
+      sortByDirection = s.settings('autoCurveSortByDirection'),
+      defaultEdgeType = s.settings('defaultEdgeType'),
+      edges = s.graph.edges();
+
     var count = {
       key: function(o) {
         var key = o.source + ',' + o.target;
         if (this[key]) {
           return key;
         }
-        key = o.target + ',' + o.source;
-        if (this[key]) {
-          return key;
+        if (!sortByDirection) {
+          key = o.target + ',' + o.source;
+          if (this[key]) {
+            return key;
+          }
         }
         this[key] = { i: 0, n: 0 };
         return key;
@@ -32,34 +55,15 @@
       }
     };
 
-    // curvature coefficients
-    var cc = {
-      ratio: ratio || 0.125,
-      step: function(len, n) {
-        if (n % 2 === 0) {
-          n--;
-        }
-        return len / (0.5 * n);
-      },
-      calc: function(len, step, i) {
-        var d = len - step * i;
-        return { y: d ? 1/d : d };
-      }
-    };
-
-    var defaultEdgeType = s.settings('defaultEdgeType');
-    var edges = s.graph.edges();
-
     edges.forEach(function(edge) {
       count.inc(edge);
     });
-
-    var key, step;
 
     edges.forEach(function(edge) {
       key = count.key(edge);
 
       if (count[key].n > 1) {
+        // update edge type:
         if (edge.type === 'arrow' || edge.type === 'tapered' ||
           defaultEdgeType === 'arrow' || defaultEdgeType === 'tapered') {
 
@@ -71,13 +75,8 @@
           edge.type = 'curve';
         }
 
-        step = cc.step(cc.ratio, count[key].n);
-
-        edge.cc = sigma.utils.extend(
-          {},
-          cc.calc(cc.ratio, step, count[key].i++),
-          cc
-        );
+        // curvature coefficients
+        edge.cc = calc(ratio, count[key].n, count[key].i++, sortByDirection);
       }
       else if (edge.cc) {
         // the edge is no longer a parallel edge
